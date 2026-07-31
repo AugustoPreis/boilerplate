979a1a556aed78ab2338e25f8e63c3099659ebea
Augusto Tomasi <augusto.tomasi@innspire.dev>
2026-07-31 16:34:29 -0300

    feat(shared): add user property extractor in CurrentUser decorator

--- apps/api/src/shared/decorators/current-user.decorator.ts
+++ apps/api/src/shared/decorators/current-user.decorator.ts
@@ -1,7 +1,13 @@
 import { ExecutionContext, createParamDecorator } from '@nestjs/common';
 
-export const CurrentUser = createParamDecorator((_data: unknown, ctx: ExecutionContext) => {
-  const request = ctx.switchToHttp().getRequest<{ user: unknown }>();
+export const CurrentUser = createParamDecorator(
+  (data: string | undefined, ctx: ExecutionContext) => {
+    const { user } = ctx.switchToHttp().getRequest();
 
-  return request.user;
-});
+    if (!data) {
+      return user;
+    }
+
+    return user?.[data];
+  },
+);
